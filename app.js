cf92ec0498be1538cd4befb31e0814cd7368d8c5
Jason Ma <jsma@mit.edu>
2016-01-17 21:16:53 -0500

    Tried to implement file upload
    I am noob

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var multer = require('multer');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -14,8 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//ap.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(multer({dest:'./uploads/'}).single('report'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +58,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
+//upload
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Report Cards' });
 });
 
+router.post('/upload', function(req, res) {
+  console.dir(req.files);
+});
 module.exports = router;
