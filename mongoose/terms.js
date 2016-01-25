a47a61b8571f610cd770ac502dd30600048072c1
kimleon <kimleon@mit.edu>
2016-01-24 21:14:55 -0500

    filepicker upload grades

--- mongoose/terms.js
+++ mongoose/terms.js
@@ -5,6 +5,7 @@ var termSchema = Schema({
     schoolYear: {type: String, required: true},
     period: {type: String, required: true},
     students: [{type: Schema.Types.ObjectId, ref: 'Student'}],
+    reportCards: {type: String, required: true},
 });
 
 var Term = mongoose.model('Term', termSchema);
